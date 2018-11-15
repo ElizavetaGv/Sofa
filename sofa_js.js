8e60a44cd0604fefc6022fcd8397d4f1d7c36c8e
ElizavetaGv <geand99@yandex.ru>
2018-11-15 20:58:43 +0700

    Версия телефона

--- sofa_js.js
+++ sofa_js.js
@@ -11,40 +11,54 @@ function onDrag(event) {
 document.getElementById('sofa').addEventListener('dragstart', onDrag);
 
 document.getElementById('sofa').addEventListener('mousedown',onStart);
+document.getElementById('sofa').addEventListener('touchstart',onStart);
 
 function onStart(event){
-	startX=event.pageX;
-	startY=event.pageY;
+	if (event.touches!=underfined) {
+		startX=event.touches[0].pageX;
+		startY=event.touches[0].pageY;
+	} else {
+		startX=event.pageX;
+		startY=event.pageY;
+	}
 	window.addEventListener('mousemove',onMove);
-  window.addEventListener('mouseup',onEnd);
+	window.addEventListener('touchmove',onMove);
+	window.addEventListener('mouseup',onEnd);
+	window.addEventListener('touchend',onEnd);
 }
 var maxX=document.getElementById('room').offsetWidth-200;
 var maxY=document.getElementById('room').offsetHeight-88;
 function onMove(event){
-	var deltaX=event.pageX-startX;
-	var deltaY=event.pageY-startY;
+	if (event.touches!=underfined) {
+		var deltaX=event.touches[0].pageX-startX;
+		var deltaY=event.touches[0].pageY-startY;
+	} else {
+		var deltaX=event.pageX-startX;
+		var deltaY=event.pageY-startY;
+	}
 	currentX=sofaX+deltaX;
 	currentY=sofaY+deltaY;
 	if (currentY>maxY) {
 		currentY=maxY;
-  }
-  if (currentX>maxX) {
-  	currentX=maxX;
-  }
-  if (currentX<0){
- 	 currentX=0;
-  }
-  if (currentY<0){
- 	 currentY=0;
-  }
-  document.getElementById('sofa').setAttribute('style','left:'+ currentX+'px; top:'+ currentY+'px');
-  
+	}
+	if (currentX>maxX) {
+		currentX=maxX;
+	}
+	if (currentX<0){
+		currentX=0;
+	}
+	if (currentY<0){
+		currentY=0;
+	}
+	document.getElementById('sofa').setAttribute('style','left:'+ currentX+'px; top:'+ currentY+'px');
 }
 function onEnd(event){
 	sofaX=currentX;
-  sofaY=currentY;
+	sofaY=currentY;
   
-  document.getElementById('sofa').setAttribute('style','left:'+ sofaX+'px; top:'+ sofaY+'px');
-  window.removeEventListener('mousemove', onMove);
-  window.removeEventListener('mouseup', onEnd);
+	document.getElementById('sofa').setAttribute('style','left:'+ sofaX+'px; top:'+ sofaY+'px');
+	window.removeEventListener('mousemove', onMove);
+	window.removeEventListener('touchmove', onMove);
+	window.removeEventListener('mouseup', onEnd);
+	window.removeEventListener('touchend', onEnd);
 }
